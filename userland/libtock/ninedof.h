53ae624c309e43b401cc0211b9096ba44eccb6a7
Brad Campbell <bradjc5@gmail.com>
2017-01-12 13:31:03 -0500

    Add virtualization layer for 9DOF sensors
    This lets multiple apps use the FXOS8700.
    
    Userland is updated to use a generic "ninedof" driver rather than a
    specific one for each IC.
    
    This also fixes naming by removing a spurious _ that appeared in the fxo
    driver filename.

--- userland/libtock/ninedof.h
+++ userland/libtock/ninedof.h
@@ -6,22 +6,23 @@
 extern "C" {
 #endif
 
-#define DRIVER_NUM_FXO 11
+#define DRIVER_NUM_NINEDOF 11
 
 // Proivide a callback function for acceleration readings
-int FXOS8700CQ_subscribe(subscribe_cb callback, void* userdata);
+int ninedof_subscribe(subscribe_cb callback, void* userdata);
 // Read acceleration and relay to callback function
-int FXOS8700CQ_start_accel_reading(void);
+
+int ninedof_start_accel_reading(void);
 // Read magnetometer and relay to callback function
-int FXOS8700CQ_start_magnetometer_reading(void);
+int ninedof_start_magnetometer_reading(void);
 // Read square of magnitude of acceleration (blocking)
-double FXOS8700CQ_read_accel_mag(void);
+double ninedof_read_accel_mag(void);
 
 // Get the magnitude of acceleration in the X,Y,Z directions. Blocking.
-int FXOS8700CQ_read_acceleration_sync(int* x, int* y, int* z);
+int ninedof_read_acceleration_sync(int* x, int* y, int* z);
 
 // Get a reading from the magnetometer. Blocking.
-int FXOS8700CQ_read_magenetometer_sync(int* x, int* y, int* z);
+int ninedof_read_magenetometer_sync(int* x, int* y, int* z);
 
 #ifdef __cplusplus
 }
